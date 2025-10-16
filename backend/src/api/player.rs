04c0f633b26d882a0e8b5ffcecd86e968b6cbdf5
PravinViswa <pravinviswa7601@gmail.com>
2025-10-16 14:08:07 +0530

    shuttle backend setup code changes

--- backend/src/api/player.rs
+++ backend/src/api/player.rs
@@ -10,32 +10,33 @@ pub struct CreatePlayerRequest{
     player_id: String,
     passcode: String,
     #[serde(default="default_grid_dimension")]
-    grid_width:usize,
+    grid_width: usize,
     #[serde(default="default_grid_dimension")]
     grid_height:usize,
 }
 
-fn default_grid_dimension()->usize{4}
+fn default_grid_dimension()->usize{
+    4
+}
 
 pub async fn create_player(
-    info:web::Json<CreatePlayerRequest>,
-    db_client:web::Data<DynamoDbClient>,
-)->impl Responder{
+    info: web::Json<CreatePlayerRequest>,
+    db_client: web::Data<DynamoDbClient>,
+) -> impl Responder{
     let player_id=info.player_id.clone();
-
     match db_client.load_game(&player_id).await{
         Ok(Some(_))=>{
             return HttpResponse::Conflict().body("Player ID already exists. Please choose a different ID.");
         }
-        Ok(None)=>{},
-        Err(e)=>return HttpResponse::InternalServerError().body(format!("Database error:{}",e)),
+        Ok(None)=>{}
+        Err(e)=>return HttpResponse::InternalServerError().body(format!("Database error: {}",e)),
     }
-    
+
     let mut new_game=GameState::new(
-        player_id, 
+        player_id,
         info.passcode.clone(),
         info.grid_width,
-        info.grid_height
+        info.grid_height,
     );
 
     let board_with_first_tile=add_random_tile(&new_game.board);
@@ -45,4 +46,11 @@ pub async fn create_player(
         Ok(_)=>HttpResponse::Created().json(new_game),
         Err(e)=>HttpResponse::InternalServerError().body(format!("Failed to save new player:{}",e)),
     }
+}
+
+pub fn configure_routes(cfg:&mut web::ServiceConfig){
+    cfg.service(
+        web::scope("/player")
+            .route("/create", web::post().to(create_player)),
+    );
 }
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,48 +1,60 @@
-mod config;
 mod api;
+mod config;
 mod models;
 mod services;
 mod utils;
 
-use actix_web::{web, App, HttpServer};
 use actix_cors::Cors;
-use api::game;
-use api::player;
-use services::storage::DynamoDbClient;
-use std::env;
+use actix_web::{middleware::Logger,web,App,HttpServer};
+use api::{game, player};
 use aws_config;
+use services::storage::DynamoDbClient;
+use shuttle_actix_web::ShuttleActixWeb;
+use shuttle_runtime::SecretStore;
+use std::sync::{Arc,Mutex};
 
-#[actix_web::main]
-async fn main()->std::io::Result<()>{
-    dotenv::dotenv().ok();
-    env::var("AWS_REGION").expect("AWS_REGION must be set in the .env file");
+fn app_config(cfg:&mut web::ServiceConfig){
+    let cors=Cors::default()
+        .allow_any_origin()
+        .allow_any_method()
+        .allow_any_header()
+        .max_age(3600);
 
-    let shared_config=aws_config::load_from_env().await;
+    cfg.service(
+        web::scope("")
+            .wrap(cors)
+            .wrap(Logger::default())
+            .configure(player::configure_routes)
+            .configure(game::configure_routes),
+    );
+}
+
+//Shuttle's main macro
+#[shuttle_runtime::main]
+async fn actix_web(
+    #[shuttle_runtime::Secrets] secrets:SecretStore,
+)->ShuttleActixWeb<impl FnOnce(&mut web::ServiceConfig)+Send+Clone+'static>{
+    
+    //To Load secrets from Shuttle's secret store
+    let aws_region=secrets.get("AWS_REGION").expect("AWS_REGION must be set");
+    let aws_access_key_id=secrets.get("AWS_ACCESS_KEY_ID").expect("AWS_ACCESS_KEY_ID must be set");
+    let aws_secret_access_key= secrets.get("AWS_SECRET_ACCESS_KEY").expect("AWS_SECRET_ACCESS_KEY must be set");
+
+    //Set environment variables for the AWS SDK to use
+    std::env::set_var("AWS_REGION",aws_region);
+    std::env::set_var("AWS_ACCESS_KEY_ID",aws_access_key_id);
+    std::env::set_var("AWS_SECRET_ACCESS_KEY",aws_secret_access_key);
+    
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    let shared_config=aws_config::load_defaults(aws_config::BehaviorVersion::latest()).await;
     let db_client=aws_sdk_dynamodb::Client::new(&shared_config);
     let dynamo_service=DynamoDbClient::new(db_client);
     let dynamo_data=web::Data::new(dynamo_service);
 
-    let host="0.0.0.0";
-    let port=8080;
-    println!("Server running at http://{}:{}",host,port);
+    let config=move|cfg:&mut web::ServiceConfig|{
+        cfg.app_data(dynamo_data.clone()).configure(app_config);
+    };
 
-    HttpServer::new(move||{
-        let cors=Cors::default()
-              .allowed_origin("http://localhost:3000")
-              .allowed_methods(vec!["GET","POST"])
-              .allow_any_header()
-              .max_age(3600);
-
-        App::new()
-            .wrap(cors)
-            .app_data(dynamo_data.clone()) 
-            .configure(game::configure_routes)
-            .service(
-                web::scope("/player")
-                    .route("/create",web::post().to(player::create_player))
-            )
-    })
-    .bind((host,port))?
-    .run()
-    .await
+    Ok(config.into())
 }
